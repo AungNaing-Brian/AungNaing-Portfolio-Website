8669421d7fa43529b89634ad069781929cbaa059
AungNaing <aungnaing.brina@gmail.com>
2025-11-19 16:39:29 +0400

    addedHeader

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 /* ========== SIDEBAR ELEMENTS ========== */
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
 
 /* ========== SIDEBAR ELEMENTS ========== */
+
+menu.addEventListener("click", function(){
+    sideBar.classList.remove("close-sidebar")
+    sideBar.classList.add("open-sidebar")
+});
+
+closeIcon.addEventListener("click", function(){
+    sideBar.classList.remove("open-sidebar");
+    sideBar.classList.add("close-sidebar");
+    
+})
 
 /* ========== MAIL FUNCTION ========== */
