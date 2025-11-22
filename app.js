375a10645178d887c31698c19dd2f1c79ab59763
AungNaing <aungnaing.brina@gmail.com>
2025-11-22 22:35:49 +0400

    finalPolish

--- app.js
+++ app.js
@@ -1,51 +1,40 @@
 /* ========== SIDEBAR ELEMENTS ========== */
-const sideBar = document.querySelector('.sidebar');
-const menu = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.close-icon')
+const sideBar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close-icon");
 
 /* ========== SIDEBAR ELEMENTS ========== */
 
-menu.addEventListener("click", function(){
-    sideBar.classList.remove("close-sidebar")
-    sideBar.classList.add("open-sidebar")
+menu.addEventListener("click", function () {
+  sideBar.classList.remove("close-sidebar");
+  sideBar.classList.add("open-sidebar");
 });
 
-closeIcon.addEventListener("click", function(){
-    sideBar.classList.remove("open-sidebar");
-    sideBar.classList.add("close-sidebar");
-    
-})
+closeIcon.addEventListener("click", function () {
+  sideBar.classList.remove("open-sidebar");
+  sideBar.classList.add("close-sidebar");
+});
 
 /* ========== MAIL FUNCTION ========== */
 
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value,
-    }
+function sendMail(event) {
+  event.preventDefault(); 
 
-    emailjs.send("service_mmpowlf", "template_9qn3pbp", parms)
-    .then(() => {
-        Swal.fire({
-            title: "Message Sent!",
-            text: "Thank you for contacting me 😊",
-            icon: "success",
-            showClass: {
-                popup: `
-                  animate__animated
-                  animate__fadeInDown
-                `
-            },
-            hideClass: {
-                popup: `
-                  animate__animated
-                  animate__fadeOutUp
-                `
-            }
-        });
-        document.getElementById("contact-form").reset();
+  let parms = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs.send("service_mmpowlf", "template_9qn3pbp", parms).then(() => {
+    Swal.fire({
+      title: "Message Sent!",
+      text: "Thank you for contacting me 😊",
+      icon: "success",
     });
+
+    document.getElementById("contact-form").reset();
+  });
 }
 
 /* ========== FOR PROJECT PAGE/PANDING MESSAGE ========== */
@@ -54,6 +43,6 @@ function pendingProject() {
     icon: "info",
     title: "Coming Soon!",
     text: "This project is still in progress. Stay tuned!",
-    confirmButtonColor: "#6a00ff"
+    confirmButtonColor: "#6a00ff",
   });
 }
